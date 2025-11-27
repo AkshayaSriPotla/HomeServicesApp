982fab0835ea24bb2f424a7c41a6bb0a5066edc7
Akshaya Sri Potla <akshayasripotla@gmail.com>
2025-11-27 20:48:05 +0530

    first commit

--- backend/HomeServicesApplication/src/main/java/com/hss/repository/ContactFormRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hss.repository;
-
-import com.hss.model.ContactForm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactFormRepository extends JpaRepository<ContactForm, Long> {
-}
